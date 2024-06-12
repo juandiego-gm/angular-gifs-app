cba42991f4af03b5ac01185f8f15fbda75675898
Juan Diego <juandiego-gm@hotmail.com>
2024-06-11 21:50:26 -0500

    Fin sección 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     GifsModule,
     SharedModule,
--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { afterRender, Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { after } from 'node:test';
 
 @Component({
   selector: 'gifs-search-box',
@@ -17,8 +19,11 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor( private gifsService: GifsService ) {}
+
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({newTag});
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
   }
 }
